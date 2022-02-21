0bb93399f3a9570e3ea18200b5faa73727fd6a28
jacques-andre <jacquesandre@mailbox.org>
2022-02-21 14:29:04 +0000

    Using struct methods rather than passing board

--- game.go
+++ game.go
@@ -1,92 +1,87 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 type Board struct {
-  BoardArray [9][9]int
+	BoardArray [9][9]int
 }
 
 // Generates a valid board with some blank spaces,
 // user playable board
-func newPlayingBoard() *Board{
-  // Create an inital test board
-  validBoard := [9][9]int{
-    {3, 0, 6, 5, 0, 8, 4, 0, 0},
-    {5, 2, 0, 0, 0, 0, 0, 0, 0},
-    {0, 8, 7, 0, 0, 0, 0, 3, 1},
-    {0, 0, 3, 0, 1, 0, 0, 8, 0}, 
-    {9, 0, 0, 8, 6, 3, 0, 0, 5}, 
-    {0, 5, 0, 0, 9, 0, 6, 0, 0}, 
-    {1, 3, 0, 0, 0, 0, 2, 5, 0}, 
-    {0, 0, 0, 0, 0, 0, 0, 7, 4}, 
-    {0, 0, 5, 2, 0, 6, 3, 0, 0},
-  } 
-  // Return new Board struct with board ^ as value
-  return &Board{
-    BoardArray: validBoard,
-  }
+func newPlayingBoard() *Board {
+	// Create an inital test board
+	validBoard := [9][9]int{
+		{3, 0, 6, 5, 0, 8, 4, 0, 0},
+		{5, 2, 0, 0, 0, 0, 0, 0, 0},
+		{0, 8, 7, 0, 0, 0, 0, 3, 1},
+		{0, 0, 3, 0, 1, 0, 0, 8, 0},
+		{9, 0, 0, 8, 6, 3, 0, 0, 5},
+		{0, 5, 0, 0, 9, 0, 6, 0, 0},
+		{1, 3, 0, 0, 0, 0, 2, 5, 0},
+		{0, 0, 0, 0, 0, 0, 0, 7, 4},
+		{0, 0, 5, 2, 0, 6, 3, 0, 0},
+	}
+	// Return new Board struct with board ^ as value
+	return &Board{
+		BoardArray: validBoard,
+	}
 }
-
 
 // Check if cellVal can be placed in the row rowN
 // Returns true if valid pos
-func validPosInRow(board *Board, cellVal int, rowN int) bool{
-  for i := 0; i < len(board.BoardArray[rowN]); i++{
-    currentValue := &board.BoardArray[rowN][i]
-    if *currentValue == cellVal{
-      return false
-    }
-  }
-  return true;
+func (board *Board) validPosInRow(cellVal int, rowN int) bool {
+	for i := 0; i < len(board.BoardArray[rowN]); i++ {
+		currentValue := &board.BoardArray[rowN][i]
+		if *currentValue == cellVal {
+			return false
+		}
+	}
+	return true
 }
 
 // Check if cellVal can be placed in the column colN
 // Returns true if valid pos
-func validPosInCol(board *Board, cellVal int, colN int) bool{
-  for i := 0; i < len(board.BoardArray[colN]); i++{
-    currentValue := &board.BoardArray[i][colN]
-    if *currentValue == cellVal{
-      return false
-    }
-  }
-  return true;
+func (board *Board) validPosInCol(cellVal int, colN int) bool {
+	for i := 0; i < len(board.BoardArray[colN]); i++ {
+		currentValue := &board.BoardArray[i][colN]
+		if *currentValue == cellVal {
+			return false
+		}
+	}
+	return true
 }
-
 
 // Check if cellVal can be placed in the 3x3 subgrid
 // Returns true if valid pos
-func validPosInSubGrid(board *Board, cellVal int, colN int, rowN int) bool{
-    rowStart := (rowN / 3) * 3
-    colStart := (colN / 3) * 3
+func (board *Board) validPosInSubGrid(cellVal int, colN int, rowN int) bool {
+	rowStart := (rowN / 3) * 3
+	colStart := (colN / 3) * 3
 
-    for i := colStart; i < colStart + 3; i++{
-      for j := rowStart; j < rowStart + 3; j++{
-        subGridCell := &board.BoardArray[i][j]
-        fmt.Println(*subGridCell)
-          if *subGridCell == cellVal {
-            return false;
-          }
-      }
-    }
-    return true;
+	for i := colStart; i < colStart+3; i++ {
+		for j := rowStart; j < rowStart+3; j++ {
+			subGridCell := &board.BoardArray[i][j]
+			if *subGridCell == cellVal {
+				return false
+			}
+		}
+	}
+	return true
 
 }
-
 
 // Helper Method:
 // Prints board nicely
-func printBoard(board *Board){
-  for _, v := range board.BoardArray{
-    fmt.Println(v)
-  }
+func printBoard(board *Board) {
+	for _, v := range board.BoardArray {
+		fmt.Println(v)
+	}
 }
 
+func main() {
+	board := newPlayingBoard()
 
-
-
-func main(){
-  board := newPlayingBoard()
-  printBoard(board)
+	// place(board)
+	printBoard(board)
 }
--- game_test.go
+++ game_test.go
@@ -5,74 +5,70 @@ import (
 	"testing"
 )
 
-func TestValidInBoardRow(t *testing.T){
-  board := newPlayingBoard()
+func TestValidInBoardRow(t *testing.T) {
+	board := newPlayingBoard()
 
-  // Test vars
-  valueN := 9
-  rowN := 0
+	// Test vars
+	valueN := 9
+	rowN := 0
 
-  got := validPosInRow(board, valueN, rowN)
-  want := true
-  
-  fmt.Printf("Row:%v, checking:%d", board.BoardArray[rowN], valueN)
+	got := board.validPosInRow(valueN, rowN)
+	want := true
 
+	fmt.Printf("Row:%v, checking:%d", board.BoardArray[rowN], valueN)
 
-  if got != want {
-    t.Errorf("Failed! Got:%t, want:%t", got, want)
-  }
+	if got != want {
+		t.Errorf("Failed! Got:%t, want:%t", got, want)
+	}
 }
 
-func TestInValidInBoardRow(t *testing.T){
-  board := newPlayingBoard()
-  // Test vars
-  valueN := 3
-  rowN := 0
+func TestInValidInBoardRow(t *testing.T) {
+	board := newPlayingBoard()
+	// Test vars
+	valueN := 3
+	rowN := 0
 
-  got := validPosInRow(board, valueN, rowN)
-  want := false
-  
-  fmt.Printf("Row:%v, checking:%d", board.BoardArray[rowN], valueN)
+	got := board.validPosInRow(valueN, rowN)
+	want := false
 
+	fmt.Printf("Row:%v, checking:%d", board.BoardArray[rowN], valueN)
 
-  if got != want {
-    t.Errorf("Failed! Got:%t, want:%t", got, want)
-  }
+	if got != want {
+		t.Errorf("Failed! Got:%t, want:%t", got, want)
+	}
 }
 
-func TestValidInBoardCol(t *testing.T){
-  board := newPlayingBoard()
-  // Test vars
-  valueN := 7
-  colN := 0
+func TestValidInBoardCol(t *testing.T) {
+	board := newPlayingBoard()
+	// Test vars
+	valueN := 7
+	colN := 0
 
-  got := validPosInCol(board, valueN, colN)
-  want := true
-  
-  printBoard(board)
-  fmt.Printf("Checking:%d, col:%d", valueN, colN)
+	got := board.validPosInCol(valueN, colN)
+	want := true
 
+	printBoard(board)
+	fmt.Printf("Checking:%d, col:%d", valueN, colN)
 
-  if got != want {
-    t.Errorf("Failed! Got:%t, want:%t", got, want)
-  }
+	if got != want {
+		t.Errorf("Failed! Got:%t, want:%t", got, want)
+	}
 }
 
-func TestInValidInBoardCol(t *testing.T){
-  board := newPlayingBoard()
+func TestInValidInBoardCol(t *testing.T) {
+	board := newPlayingBoard()
 
-  // Test vars
-  valueN := 9
-  colN := 0
+	// Test vars
+	valueN := 9
+	colN := 0
 
-  got := validPosInCol(board, valueN, colN)
-  want := false
-  
-  fmt.Printf("Checking:%d, col:%d", valueN, colN)
-  printBoard(board)
+	got := board.validPosInCol(valueN, colN)
+	want := false
 
+	fmt.Printf("Checking:%d, col:%d", valueN, colN)
+	printBoard(board)
 
-  if got != want {
-    t.Errorf("Failed! Got:%t, want:%t", got, want)
-  }
+	if got != want {
+		t.Errorf("Failed! Got:%t, want:%t", got, want)
+	}
 }
