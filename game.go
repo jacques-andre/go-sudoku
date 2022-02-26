3595c92999d57255310ae22c1c5e1cfef569a2db
jacques-andre <jacquesandre@mailbox.org>
2022-02-26 17:03:02 +0000

    Able to select pos on UserBoard

--- game.go
+++ game.go
@@ -13,23 +13,29 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	board := board.Board{SolvedBoard: grid, UserBoard: grid}
 	board.GenerateBoard(0, 0)
-	board.GenerateUserBoard(20)
+	board.GenerateUserBoard(70)
 
-	count := 0
+  // holds user selection
+  var inputRow int
+  var inputCol int
 
-	fmt.Printf("User board:  \n")
-	for row := 0; row < len(board.UserBoard); row++ {
-		for col := 0; col < len(board.UserBoard[row]); col++ {
-			fmt.Printf("%d|", board.UserBoard[row][col])
-			if board.UserBoard[row][col] == 0 {
-				count++
-			}
-		}
-		fmt.Println()
-	}
-	fmt.Printf("zeros:%d \n", count)
 
-	fmt.Printf("Solved board: \n")
-	board.PrintBoard()
+  for row := 0; row < len(board.UserBoard); row++{
+    for col := 0; col < len(board.UserBoard[row]); col++{
+      fmt.Printf("%d|", board.UserBoard[row][col])
+    }
+    fmt.Println()
+  }
+
+
+
+  fmt.Printf("Choose a row:\n")
+  fmt.Scanln(&inputRow)
+
+  fmt.Printf("Choose a col:\n")
+  fmt.Scanln(&inputCol)
+
+  fmt.Printf("You choose row:%v, col:%v, val:%d \n", inputRow, inputCol, board.UserBoard[inputRow][inputCol])
+  board.PrintBoard()
 
 }
